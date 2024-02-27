86eae4bce8dc252450a2799fcf28db3d8cb602a4
kats <temjaitnt@gmail.com>
2024-02-27 20:22:36 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+const Discord = require("discord.js-selfbot-v13");
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+    checkUpdate: false
+});
+const express = require('express')
+const app = express();
+const port = 8000;
+
+const largeImages = [
+    'https://media.discordapp.net/attachments/1205101702190202930/1212022812403834901/wiah.gif?ex=65f05334&is=65ddde34&hm=253ff9c9841f9ab328007d38ba15693e4b66e0fdba88d7ee1b5a43aaf1d1801d&=&width=547&height=311'
+    // Add more large image URLs as needed
+];
+
+const stateTexts = [
+    '「 KatV4 Raid tool! 」',
+    '「 Besser als AMW 」',
+    '「 Lang lebe die kat armee 」'
+    // Add more state texts as needed
+];
+
+const nameTexts = [
+  '꒦꒷ KatV4 Raid tool! ',
+  '꒦꒷ Besser als AMW ',
+  '꒦꒷ Lang lebe die kat armee '
+  // Add more state texts as needed
+];
+
+
+let currentStateIndex = 0; // Index to track the current state text
+
+let currentLargeImageIndex = 0;
+
+let currentnameTextsIndex = 0;
+
+app.get('/', (req, res) => res.send('ทำงานเรียบร้อยแล้ว'))
+app.listen(port, () =>
+  console.log(`Your app is listening at http://localhost:${port}`)
+);
+
+client.on("ready", async () => {
+  var startedAt = Date.now();
+  console.log(`${client.user.username} เม็ดม่วงทำงานเรียบร้อยแล้ว !`);
+
+  setInterval(() => {
+      const currentTime = getCurrentTime();
+      const currentDate = getCurrentDate();
+
+      const r = new Discord.RichPresence()
+          .setApplicationId('1121867777867788309')
+          .setType('STREAMING')
+          .setURL('https://youtu.be/sVaQQRx6-es?si=WddbMqrjlhmF6kF8')
+          .setState(stateTexts[currentStateIndex])
+          .setName(nameTexts[currentnameTextsIndex])
+          .setDetails(` ﹝ ⌚ ${currentTime} | 🌌 ${client.user.username} ﹞ `)
+          .setStartTimestamp(startedAt)
+          .setAssetsLargeText(`﹝ 📅 ${currentDate}  | 🛸 0 m/s ﹞`)
+          .setAssetsLargeImage(largeImages[currentLargeImageIndex])
+          .setAssetsSmallText('🦊')
+          .addButton('About Us ', 'https://discord.com/invite/xhqcSC5K4Y')
+          .addButton('Our Clan', 'https://discord.gg/w00d')
+
+      client.user.setActivity(r);
+
+      currentLargeImageIndex = (currentLargeImageIndex + 1) % largeImages.length;
+      currentStateIndex = (currentStateIndex + 1) % stateTexts.length;
+      currentnameTextsIndex = (currentnameTextsIndex + 1) % nameTexts.length;
+  }, 3000); // Change large image and state text every 1 second
+});
+
+function getCurrentDate() {
+  const a = new Date(Date.now());
+  const c = { timeZone: "Asia/Bangkok", day: "2-digit", month: "2-digit", year: "numeric" };
+  const formattedDate = a.toLocaleDateString("en-US", c);
+  const [month, day, year] = formattedDate.split('/');
+  return `${day}/${month}/${year}`;
+}
+
+function getCurrentTime() {
+  const a = new Date(Date.now());
+  const c = { timeZone: "Asia/Bangkok", hour: "numeric", minute: "numeric", hour12: false };
+  return a.toLocaleTimeString("th-TH", c);
+}
+
+client.login(process.env.token);
